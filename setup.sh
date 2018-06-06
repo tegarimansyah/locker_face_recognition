c8fc31495682c92332b11dfd4b0cd5102a23ff74
Tegar Imansyah <egar.imansyah@gmail.com>
2018-06-06 08:58:20 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential \
+    cmake \
+    gfortran \
+    git \
+    wget \
+    curl \
+    graphicsmagick \
+    libgraphicsmagick1-dev \
+    libatlas-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libboost-all-dev \
+    libgtk2.0-dev \
+    libjpeg-dev \
+    liblapack-dev \
+    libswscale-dev \
+    pkg-config \
+    python3-dev \
+    python3-numpy \
+    python3-pip \
+    zip
+sudo apt-get clean
